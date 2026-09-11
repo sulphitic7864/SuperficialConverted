b6d69328e33da792d9c0118a5c8cda9779e71ce8
Waseem Qasim <wqasimg4@gmail.com>
2026-09-11 21:10:08 +0500

    backend is updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -32,7 +32,6 @@ app.use(
     },
   }),
 );
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+import "dotenv/config";
 import app from "./app";
 import { logger } from "./lib/logger";
 
